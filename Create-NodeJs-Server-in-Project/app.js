7680f4d724f5b34c87835165fbe63d8a7df3bdc5
Osman Forhad <osmanforhad93@gmail.com>
2018-09-06 22:41:42 +0600

    Update app.js
    Write Some comment for Understanding of code

--- Create-NodeJs-Server-in-Project/app.js
+++ Create-NodeJs-Server-in-Project/app.js
@@ -1,5 +1,7 @@
 var http = require('http');
+// Define The Port Number of Server
 var port = 3000;
+//Create a function With 2parameters and Descrive the type of Server Content
 var myFunction = function(request, response){
   response.writeHead(200, {'content-type':'text/html'});
   response.write("<h1>Welcome Developer This is youe first NodeJs Task</h1>");
@@ -7,4 +9,5 @@ var myFunction = function(request, response){
 };
 var server = http.createServer(myFunction);
 server.listen(port);
+//Write a Message for Display The Port Number of Server Which given into the project Within the variavle ot var port
 console.log("Your Server is Working on port no :" +port);
